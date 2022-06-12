d8aaf485c051cb40fb0b1fa99f58276d97ed8e82
SuperYing <13975145507@163.com>
2022-06-12 22:13:53 +0800

    feat: app  main 页面

--- src/layout/app-main.vue
+++ src/layout/app-main.vue
@@ -6,8 +6,8 @@
 <template>
   <section class="app-main">
     <router-view v-slot="{ Component }">
-      <transition>
-        <keep-alive>
+      <transition name="fade" mode="out-in">
+        <keep-alive :include="cachedView">
           <component :is="Component"></component>
         </keep-alive>
       </transition>
@@ -15,8 +15,12 @@
   </section>
 </template>
 <script setup lang="ts">
+import { ref } from 'vue'
 // eslint-disable-next-line no-undef
 defineOptions({
   name: 'AppMain',
 })
+
+// 缓存的页面组件
+const cachedView = ref([])
 </script>
