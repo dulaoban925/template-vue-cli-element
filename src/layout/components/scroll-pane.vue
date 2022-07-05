114ba70513ca2990b3b2bb2a48ce6854dc89bf8c
SuperYing <13975145507@163.com>
2022-07-05 21:41:17 +0800

    feat: Srollpane 基础结构

--- src/layout/components/scroll-pane.vue
+++ src/layout/components/scroll-pane.vue
@@ -4,13 +4,30 @@
   @date 2022/07/02 14:16:51
  -->
 <template>
-  <div class="scroll-pane">
+  <el-scrollbar ref="scrollPaneRef" :class="ns.b()" @scroll="handleScroll">
     <slot />
-  </div>
+  </el-scrollbar>
 </template>
 <script setup lang="ts">
+import { ref, defineExpose } from 'vue'
+import { useNamespace } from '@/hooks'
 // eslint-disable-next-line no-undef
 defineOptions({
   name: 'ScrollPane',
 })
+
+const ns = useNamespace('scroll-pane')
+const scrollPaneRef = ref(null)
+
+// 处理滚动
+const handleScroll = ({ scrollLeft, scrollTop }: { scrollLeft: number; scrollTop: number }) => {
+  console.log(scrollLeft, scrollTop)
+}
+
+// 移动到指定页签
+const moveTo = (tag: HTMLElement) => {
+  console.log('move to')
+}
+
+defineExpose({ moveTo })
 </script>
